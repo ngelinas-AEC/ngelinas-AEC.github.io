597dbcc3171ab12f4521ffd3965a3b8f86aa513a
ngelinas-AEC <57510354+ngelinas-AEC@users.noreply.github.com>
2021-04-12 18:18:57 -0400

    Add files via upload
    commit

---
+++ js/install.js
@@ -0,0 +1,48 @@
+// <!-- install.js -->
+// <!-- Projet synthèse en développement WEB -->
+// <!-- Nico Gelinas -->
+
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
+
+
+//Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', afficherLebouton);
+
+function afficherLebouton(evt) {
+    //Add code to save event & show the install button.
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+}
+
+
+function installPWA(evt) {
+    // Add code show install prompt & hide the install button.
+    deferredInstallPrompt.prompt();
+    // Hide the install button, it can't be called twice.
+    evt.srcElement.setAttribute('hidden', true);
+    // Log user response to prompt.
+    deferredInstallPrompt.userChoice
+        .then((choice) => {
+            if (choice.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt', choice);
+            } else {
+                console.log('User dismissed the A2HS prompt', choice);
+            }
+            deferredInstallPrompt = null;
+        });
+}
+
+// Add event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
+/**
+ * Event handler for appinstalled event.
+ * Log the installation to analytics or save the event somehow.
+ *
+ * @param {Event} evt
+ */
+function logAppInstalled(evt) {
+// Add code to log the event
+    console.log('PintNClick App was installed via ...', evt);
+}
---
+++ precache-manifest.580e6067e5f38cc7ddd77585b534174d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bacba1b37f3cd00a0f6aef3d9463be",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/index.html"
+  },
+  {
+    "revision": "67bf52ac4c03674e708c",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "476eecbed71c55379e04",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/css/main.9aa2c4b1.chunk.css"
+  },
+  {
+    "revision": "67bf52ac4c03674e708c",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/js/2.bb96787a.chunk.js"
+  },
+  {
+    "revision": "33680cec0d310757f5a44620ee836cb0",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/js/2.bb96787a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476eecbed71c55379e04",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/js/main.d966a15a.chunk.js"
+  },
+  {
+    "revision": "17a3c437d1028e6246a0",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/js/runtime-main.8076ba60.js"
+  },
+  {
+    "revision": "2041535998fd3b7119929e871f181949",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/media/screen1_img1.20415359.png"
+  },
+  {
+    "revision": "d9bbe29db842d3cbd237b7f489819452",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/media/screen1_img6.d9bbe29d.png"
+  },
+  {
+    "revision": "832341170441dd1f90c5018be227ba72",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/media/screen1_img7.83234117.png"
+  },
+  {
+    "revision": "6b7d7b3b1cb4cd2a9bd94cd72bc15ee7",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/media/screen1_img8.6b7d7b3b.png"
+  },
+  {
+    "revision": "f4e29dfb117b4a7029bc9b1f66a0b24c",
+    "url": "/ngelinas-AEC/Projet-synthese---Portail-de-recherche-de-stage/static/media/screen1_img9.f4e29dfb.png"
+  }
+]);
